6a52b7232d95c5c4407b314d877f7a1abbd3ece7
Natnael Yeshiwas <natiyeshi16@gmail.com>
2023-08-18 08:17:42 +0300

    add admin product addition

--- client/src/Admin/pages/add-product/components/From.jsx
+++ client/src/Admin/pages/add-product/components/From.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, useRef} from 'react'
 import {GrAdd} from "react-icons/gr"
+import {FiRefreshCcw as Reset} from "react-icons/fi"
 import {checkString} from '../../../../core/functions/common'
 
 const From = () => {
@@ -8,240 +9,302 @@ const From = () => {
 
     const initialHeaders = {
         title: "",
-        tag: ""
+        tag: "",
+        price: 0
+    }
+    const initialHeadersErr = {
+        title: "",
+        tag: "",
+        price: ""
     }
     const [headers, setHeaders] = useState(initialHeaders)
-    const [headersErr, setHeadersErr] = useState(initialHeaders)
-    const initialItems = {
+    const [headersErr, setHeadersErr] = useState(initialHeadersErr)
+    const [items, setItems] = useState([])
+    const initialItem = {
         itemName: "",
         itemAmount: 0,
         img: null
     }
-    const initialItemsErr = {
+    const initialItemErr = {
         itemName: "",
         itemAmount: "",
         img: ""
     }
-    const [items, setItems] = useState(initialItems)
-    const [itemsErr, setItemsErr] = useState(initialItemsErr)
+    const [item, setItem] = useState(initialItem)
+    const [itemErr, setItemErr] = useState(initialItemErr)
+    const [itemNum, setItemNum] = useState(0)
+    const [mainData, setMainData] = useState({})
 
-    const [mainData, setMainData] = useState([])
-
-    const validateHeader = () => {
-        if (!checkString(headers.title)) {
+    const checkHeaderInput = (input) => {
+        if (!checkString(headers[input])) {
             setHeadersErr(val => ({
                 ...val,
-                title: "empty item name"
+                [input]: "empty " + input
             }))
             return false
         } else {
             setHeadersErr(val => ({
                 ...val,
-                title: ""
+                [input]: ""
             }))
         }
-
-        if (!checkString(headers.tag)) {
-            setHeadersErr(val => ({
-                ...val,
-                tag: "empty item name"
-            }))
-            return false
-        } else {
-            setHeadersErr(val => ({
-                ...val,
-                tag: ""
-            }))
-        }
-
+        return true
     }
+    const validateHeader = () => {
+        if (!(checkHeaderInput("title") && checkHeaderInput("tag"))) 
+            return false
+        
+        if (headers.price !== "" && headers.price > 0) {
+            setHeadersErr(err => ({
+                ...err,
+                price: ""
+            }))
+            return true
+        }
+        setHeadersErr(err => ({
+            ...err,
+            price: "empty price"
+        }))
+        return false
+    }
+    const validateItem = () => {
 
-    const validateItems = () => {
-
-        if (!checkString(items.itemName)) {
-            setItemsErr(val => ({
+        if (!checkString(item.itemName)) {
+            setItemErr(val => ({
                 ...val,
                 itemName: "empty item name"
             }))
             return false
         } else {
-            setItemsErr(val => ({
+            setItemErr(val => ({
                 ...val,
                 itemName: ""
             }))
         }
 
-        if (items.itemAmount < 1) {
-            setItemsErr(val => ({
+        if (item.itemAmount < 1) {
+            setItemErr(val => ({
                 ...val,
                 itemAmount: "empty amount"
             }))
             return false
         } else {
-            setItemsErr(val => ({
+            setItemErr(val => ({
                 ...val,
                 itemAmount: ""
             }))
         }
 
-        if (items.img == null) {
-            setItemsErr(val => ({
+        if (item.img == null) {
+            setItemErr(val => ({
                 ...val,
                 itemAmount: "empty image"
             }))
             return false
         } else {
-            setItemsErr(val => ({
+            setItemErr(val => ({
                 ...val,
                 img: ""
             }))
         }
+
         return true
 
     }
 
-    const submit = () => { 
-        validateHeader()
-        validateItems()
+    const changeHeader = ({target}) => setHeaders(val => ({
+        ...val,
+        [target.name]: target.value
+    }))
+
+    const submit = () => {
+
+        const checker = validateHeader() && validateItem()
+        if (checker == false) 
+            return
+        
+        setItems(data => {
+            let newDatas = [...data]
+            newDatas[itemNum] = item
+            setMainData({
+                ...headers,
+                newDatas
+            })
+            return newDatas
+        })
+
+    }
+    const addMoreItem = () => {
+        if (validateItem()) {
+            setItemNum(num => num + 1)
+            setItems(data => [
+                ...data,
+                item
+            ])
+            setItem(initialItem)
+        }
     }
 
-    return (<div className='bg-white  rounded-lg px-10 w-fit mx-auto py-5'>
-        <h3 className='text-lg my-5 font-semibold'>
-            Add your product
-        </h3>
+    const reset = () => {
+        setHeaders(initialHeaders)
+        setItem(initialItem)
+        setItems([])
+        setItemNum(0)
+    }
 
+    useEffect(() => {
+        console.log(mainData)
+    }, [mainData])
 
-        <form className='flex flex-col relative' action=""
-            onSubmit={
-                (e) => e.preventDefault()
-        }>
+    return (
+        <div className='bg-white   rounded-lg px-10 w-fit mx-auto py-5 shadow-xl'>
 
-            <div className='flex  gap-7'>
-                <div className='flex flex-col gap-2 my-auto '>
-                    <label htmlFor="" className='text-[14px]'>product title</label>
-                    <input type="text" name='title'
-                        onChange={
-                            ({target}) => setHeaders(val => ({
-                                ...val,
-                                [target.name]: target.value
-                            }))
-                        }
-                        value={
-                            headers.title
-                        }
-                        placeholder='title'
-                        className='py-1 w-60 px1 focus:outline-none bg-transparent border-b  border-gray-800 '/>
+            <div className=' flex my-5 justify-between'>
 
-                    <label htmlFor="" className='text-[14px]'>product tag</label>
-                    <input type="text" name='tag'
-                        onChange={
-                            ({target}) => setHeaders(val => ({
-                                ...val,
-                                [target.name]: target.value
-                            }))
-                        }
-                        value={
-                            headers.tag
-                        }
-                        placeholder='tag'
-                        className='py-1 w-60 px1 focus:outline-none bg-transparent border-b  border-gray-800 '/>
-
-                    <div className='text-redd'> {
-                        headersErr.title
-                    }
-                        {
-                        headersErr.tag
-                    } </div>
-
-
+                <h3 className='text-lg  font-semibold'>
+                    Add your product
+                </h3>
+                <div onClick={reset} title='reset' className='bg-gray-50 cursor-pointer    hover:bg-gray-100 p-2 rounded'>
+                    <Reset className='text-lg' />
                 </div>
 
-                <div className='flex flex-col  border w-fit  relative shadow-lg'>
-                    <div className=' bg-gray-800 text-white py-3 ps-5 font-bold'>item {
-                        mainData.length + 1
-                    }</div>
-                    <div className='px-5 flex flex-col gap-2 mt-4'>
-                        <label htmlFor="" className='text-[14px]'>item name</label>
-                        <input type="text"
-                            onChange={
-                                ({target}) => setItems({
-                                    ...items,
-                                    itemName: target.value
-                                })
-                            }
+            </div>
+
+
+            <form className='flex flex-col relative' action=""
+                onSubmit={
+                    (e) => e.preventDefault()
+            }>
+
+                <div className='flex  gap-7'>
+                    <div className='flex flex-col gap-4 my-auto '>
+                        <label htmlFor="" className='text-[14px]'>product title</label>
+                        <input type="text" name='title'
+                            onChange={changeHeader}
                             value={
-                                items.itemName
+                                headers.title
                             }
-                            placeholder='name'
-                            name='itemName'
+                            placeholder='title'
                             className='py-1 w-60 px1 focus:outline-none bg-transparent border-b  border-gray-800 '/>
-                        <label htmlFor="" className='text-[14px]'>item amount</label>
-                        <input type="number"
-                            onChange={
-                                ({target}) => setItems({
-                                    ...items,
-                                    itemAmount: target.value
-                                })
-                            }
+
+                        <label htmlFor="" className='text-[14px]'>product tag</label>
+                        <input type="text" name='tag'
+                            onChange={changeHeader}
                             value={
-                                items.itemAmount
+                                headers.tag
                             }
-                            placeholder='amount'
-                            name='itemAmount'
+                            placeholder='tag'
                             className='py-1 w-60 px1 focus:outline-none bg-transparent border-b  border-gray-800 '/>
-                        <input type="file"
-                            onChange={
-                                (event) => setItems({
-                                    ...items,
-                                    img: event.target.files[0] ?? null
-                                })
+
+                        <label htmlFor="" className='text-[14px]'>price</label>
+                        <input type="number" name='price'
+                            onChange={changeHeader}
+                            value={
+                                headers.price
                             }
-                            hidden
-                            accept='image/*'
-                            ref={imgRef}/>
+                            placeholder='price'
+                            className='py-1 w-60 px1 focus:outline-none bg-transparent border-b  border-gray-800 '/>
 
-                        <div onClick={
-                                () => imgRef.current.click()
-                            }
-                            className={
-                                `border border-gray-800  ${
-                                    items.img && "bg-gray-800 text-white "
-                                } mt-2 py-1 px-5 w-fit cursor-pointer rounded`
-                        }> {
-                            items.img ? "image added" : "Upload image"
-                        } </div>
 
-                        <div className='mb-1 py-1 text-redd'> {
-                            itemsErr.itemAmount
-                        }
+                        <div className='text-redd'>
                             {
-                            itemsErr.itemName
+                            headersErr.title + " " + headersErr.tag + " " + headersErr.price
                         }
-                            {
-                            itemsErr.image
-                        } </div>
-                        <button className='bg-transparent border font-semibold px-12 rounded py-2 mb-2 flex'>
+                            {} </div>
 
-                            <p>
-                                Add More Item
-                            </p>
-                            <GrAdd className='my-auto ms-2 '/>
-
-                        </button>
 
                     </div>
+
+                    <div className='flex flex-col  border w-fit  relative shadow-lg'>
+                        <div className=' bg-gray-800 text-white py-3 ps-5 font-bold'>item {
+                            itemNum + 1
+                        }</div>
+                        <div className='px-5 flex flex-col gap-2 mt-4'>
+                            <label htmlFor="" className='text-[14px]'>item name</label>
+                            <input type="text"
+                                onChange={
+                                    ({target}) => setItem({
+                                        ...item,
+                                        itemName: target.value
+                                    })
+                                }
+                                value={
+                                    item.itemName
+                                }
+                                placeholder='name'
+                                name='itemName'
+                                className='py-1 w-60 px1 focus:outline-none bg-transparent border-b  border-gray-800 '/>
+                            <label htmlFor="" className='text-[14px]'>item amount</label>
+                            <input type="number"
+                                onChange={
+                                    ({target}) => setItem({
+                                        ...item,
+                                        itemAmount: target.value
+                                    })
+                                }
+                                value={
+                                    item.itemAmount
+                                }
+                                placeholder='amount'
+                                name='itemAmount'
+                                className='py-1 w-60 px1 focus:outline-none bg-transparent border-b  border-gray-800 '/>
+                            <input type="file"
+                                onChange={
+                                    (event) => setItem({
+                                        ...item,
+                                        img: event.target.files[0] ?? null
+                                    })
+                                }
+                                hidden
+                                accept='image/*'
+                                ref={imgRef}/>
+
+                            <div onClick={
+                                    () => imgRef.current.click()
+                                }
+                                className={
+                                    `border border-gray-800  ${
+                                        item.img && "bg-gray-800 text-white "
+                                    } mt-2 py-1 px-5 w-fit cursor-pointer rounded`
+                            }>
+                                {
+                                item.img ? "image added" : "Upload image"
+                            } </div>
+
+                            <div className='mb-1 py-1 text-redd'>
+                                {
+                                itemErr.itemAmount
+                            }
+                                {
+                                itemErr.itemName
+                            }
+                                {
+                                itemErr.image
+                            } </div>
+                            <button onClick={addMoreItem}
+                                className='bg-transparent border font-semibold px-12 rounded py-2 mb-2 flex'>
+
+                                <p>
+                                    Add More Item
+                                </p>
+                                <GrAdd className='my-auto ms-2 '/>
+
+                            </button>
+
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className='flex mt-4 gap-1'>
+                <div className='flex mt-4 gap-1'>
 
-                <button onClick={submit}
-                    className='bg-gray-800 w-fit text-white font-semibold px-12 rounded py-2'>Finish</button>
+                    <button onClick={submit}
+                        className='bg-gray-800 w-fit text-white font-semibold px-12 rounded py-2'>Finish</button>
 
-            </div>
+                </div>
 
 
-        </form>
-    </div>)
+            </form>
+        </div>
+    )
 }
 
 export default From
