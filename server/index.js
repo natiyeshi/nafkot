760c35a8b5f8e67e760c5f39f600c7051c6d5554
Natnael Yeshiwas <natiyeshi16@gmail.com>
2023-09-03 19:48:42 +0300

    edit db config

--- server/index.js
+++ server/index.js
@@ -1,5 +1,28 @@
 require("dotenv").config()
-require("./Config/connectDb")
+
+const mongoose = require("mongoose")
+
+mongoose
+    .connect(process.env.DB_URL_REMOTE,{dbName : process.env.DB_NAME})
+    .then(() => console.log("connected to Db"))
+    .catch(err => console.log(err.message))
+
+mongoose.connection.on("connected",() => {
+    console.log("DB connection sucessfully started :)")
+})
+
+mongoose.connection.on("error",()=>{
+    console.log("connection failed :(");
+})
+
+mongoose.connection.on("disconnected",() => {
+    console.log("Db connection halted !!"); 
+}) 
+
+process.on("SIGINT",async ()=>{
+    await mongoose.connection.close() 
+    process.exit(0)
+})
 
 const express = require("express")
 const morgan = require("morgan")
