c1a035cd9de1077a82d2cae01ddc1e78d5f5804a
Natnael Yeshiwas <natiyeshi16@gmail.com>
2023-11-10 20:57:44 +0300

    upload axios base

--- client/src/Admin/hooks/axios.jsx
+++ client/src/Admin/hooks/axios.jsx
@@ -3,6 +3,7 @@ import axios from "axios"
 
 
 // axios.defaults.baseURL = 'http://localhost:4000'
-axios.defaults.baseURL = 'https://nafkot-us4u.onrender.com/'
+// axios.defaults.baseURL = 'https://nafkot-us4u.onrender.com/'
+axios.defaults.baseURL = window.location.origin;
 
 export default axios
--- client/src/core/hooks/axios.jsx
+++ client/src/core/hooks/axios.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import axios from "axios"
 
 // axios.defaults.baseURL = 'http://localhost:4000'
-axios.defaults.baseURL = 'https://nafkot-us4u.onrender.com/'
+// axios.defaults.baseURL = 'https://nafkot-us4u.onrender.com/'
+axios.defaults.baseURL = window.location.origin;
 
 export default axios
